99f42bb2f239d6c0fb93f019219c174c550318be
ymmy1 <chem-pion@mail.ru>
2020-06-25 03:06:23 -0700

    first commit

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask
+from flask import Flask, redirect, render_template, request, session, flash
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
@@ -10,4 +10,4 @@ socketio = SocketIO(app)
 
 @app.route("/")
 def index():
-    return "Project 2: TODO"
+    return render_template("index.html")
